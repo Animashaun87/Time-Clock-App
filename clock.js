8dd6b11e33abc4c009824976bbdb58c1b4bc3e06
animashaun87 <animashaunbidemi07@gmail.com>
2022-10-23 17:27:04 +0100

    worked on seconds and displayed on the browser

--- clock.js
+++ clock.js
@@ -34,8 +34,8 @@ function setTime() {
 	hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`;
 	minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`;
 	secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`;
-	timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
-	date.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+	timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds} ${ampm}`
+	dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
      
 }
 setTime()
